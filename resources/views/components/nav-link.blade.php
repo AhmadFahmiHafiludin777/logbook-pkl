9a740f62764532670742eaf71fb164994cbcdd09
fahmihafiludin123 <hafiludinfahmi354@gmail.com>
2024-11-29 14:37:28 +0700

    coba

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,4 @@
+{{-- navlink --}}
 @props(['active' => false])
 <a {{ $attributes }}class="{{ $active ? 'bg-blue-600 text-white' : 'text-gray-300 hover:bg-blue-400 hover:text-white' }}
      rounded-md  px-3 py-2 text-sm font-medium"  aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
